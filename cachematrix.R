67689b7b2638e0b9103a05f1db203cc00c80657f
akgeni <akgeni@gmail.com>
2016-05-24 22:06:16 +0530

    Update cachematrix.R
    >mat <- matrix(sample(1:100), 4, 4)
    >cacheMatrix <- makeCacheMatrix(mat)
    > cacheSolve(cacheMatrix)
    returning cached value
                 [,1]         [,2]         [,3]         [,4]
    [1,]  0.006462843  0.005974129 -0.008914954  0.007720555
    [2,] -0.024644644 -0.028614829  0.032380849  0.021962760
    [3,] -0.007580651  0.007632107  0.008218755 -0.005973892
    [4,]  0.036150980  0.019584949 -0.028537843 -0.029901312

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMat <- NULL
+  set <- function(y) {
+    x <<- y
+    invMat <<- NULL
+  }
+  
+  get <- function() x
+  setInv <- function(invMatrix) invMat <<- invMatrix
+  getInv <- function() invMat
+  
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInv()
+  if(!is.null(invMat)) {
+    message("returning cached inverse matrix")
+    return(invMat)
+  }
+  data <- x$get()
+  #print(data)
+  invMat <- solve(data, ...)
+  x$setInv(invMat)
+  invMat
+  
 }
